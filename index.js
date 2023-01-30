f551a1551b6f2cd44041421d2a28c2f89f1053d0
SinugbangIsda <marcuoperario@gmail.com>
2023-01-31 04:01:04 +0800

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require('dotenv');
+const urlRoute = require("./routes/url");
+const mongoose = require("mongoose");
+const URL = require("./models/url");
+const cors = require('cors');
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(process.env.MONGODB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true}, () => 
+    console.log("Mongo Connected"));
+
+app.use(express.json());
+app.use(cors());
+app.use('/', urlRoute);
+
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+        {
+            shortId
+        }
+    );
+    res.redirect(entry.destination);
+
+})
+app.listen(PORT, () => console.log("Server connected at port", PORT));
