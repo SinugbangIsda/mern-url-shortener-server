6eb7afff51826a925484621f378598533f462a70
SinugbangIsda <marcuoperario@gmail.com>
2023-01-31 12:19:43 +0800

    updated url routes

--- routes/url.js
+++ routes/url.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { handleGenerateNewShortURL } = require("../controllers/url");
 const router = express.Router();
 
-router.post("/",  handleGenerateNewShortURL);
+router.post("/g",  handleGenerateNewShortURL);
 
 module.exports = router;
