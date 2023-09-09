155d37deca460de8f9c6b1068ec31fb4553cef94
Marcu Operario <marcuoperario@gmail.com>
2023-09-10 04:31:31 +0800

    feat: add cron schedule to automatically delete  Shortened URls

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const urlRoute = require("./routes/url");
 const mongoose = require("mongoose");
 const URL = require("./models/url");
 const cors = require('cors');
+const cron = require('node-cron');
 dotenv.config();
 mongoose.set("strictQuery", true);
 
@@ -16,9 +17,9 @@ mongoose.connect(process.env.MONGODB_URI,
 
 app.use(express.json());
 app.use(cors());
-app.use('/g', urlRoute);
+app.use('/', urlRoute);
 
-app.get('/g/:id', async (req, res) => {
+app.get('/:id', async (req, res) => {
     const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     const entry = await URL.findOne(
         {
@@ -29,3 +30,26 @@ app.get('/g/:id', async (req, res) => {
 
 })
 app.listen(PORT, () => console.log("Server connected at port", PORT));
+
+// CRON
+const schedule = () => {
+    const cronJobPattern = '0 */2 * * *'; // This will run the task every 2 hours
+
+    const cronTask = async () => {
+        try {
+            console.log('Deleting Shortened URLs...');
+            const oneDayAgo = new Date();
+            oneDayAgo.setDate(oneDayAgo.getDate() - 1);
+            const deletedURLs = await URL.deleteMany({
+                createdAt: { $lt: oneDayAgo }
+            });
+            console.log(`Deleted ${deletedURLs.deletedCount} Shortened URLs.`);
+        } catch (error) {
+            console.error('An error occurred while deleting Shortened URLs:', error);
+        }
+    };
+
+    cron.schedule(cronJobPattern, cronTask);
+};
+
+schedule();
